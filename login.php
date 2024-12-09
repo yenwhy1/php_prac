9900ef9e71a0ba64d54a320710ce901fbef3ca1b
yenwhy1 <nicayel795@gmail.com>
2024-12-09 20:21:27 +0800

    Update login.php

--- login.php
+++ login.php
@@ -7,9 +7,3 @@ if ($_POST) {
     }
 }
 ?>
-
-<form method="POST">
-    <input type="text" name="username" placeholder="Username">
-    <input type="password" name="password" placeholder="Password">
-    <button type="submit">Login</button>
-</form>
